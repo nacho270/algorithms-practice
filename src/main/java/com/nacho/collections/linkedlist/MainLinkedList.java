4d1d542fa0d608854c9e39039314c30710bd1283
Ignacio Cicero <ignaciocicero@gmail.com>
2020-02-08 20:14:59 +0000

    Linked list

---
+++ src/main/java/com/nacho/collections/linkedlist/MainLinkedList.java
@@ -0,0 +1,27 @@
+package com.nacho.collections.linkedlist;
+
+public class MainLinkedList {
+
+	public static void main(final String[] args) {
+		final MyLinkedList<Integer> list = new MyLinkedList<>();
+		list.add(5);
+		list.add(10);
+		list.add(20);
+
+		list.print();
+
+		System.out.println("Is 10 present? " + list.isPresent(10));
+		System.out.println("Is 7 present? " + list.isPresent(7));
+		list.remove(10);
+		list.print();
+		list.insert(0, 7);
+		list.print();
+		list.insert(1, 77);
+		list.print();
+		list.insert(4, 1);
+		list.print();
+		list.reverse();
+		list.print();
+	}
+
+}
